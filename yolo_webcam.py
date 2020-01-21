bc3109f2ed7a303d4485862483a0221f015d22d4
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2020-01-21 01:00:28 -0800

    Add files via upload

--- yolo_webcam.py
+++ yolo_webcam.py
@@ -2,77 +2,81 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
-
-
 import cv2
-
-
-# In[9]:
-
-
 import numpy as np
 import time
 
+KNOWN_DISTANCE = 24.0
+KNOWN_WIDTH = 11.0
 
-# In[10]:
+def distance_to_camera(known_width, focal_length, perceived_length):
+    #computer and return the distance from the object box to camera
+    return (known_width * focal_length) / perceived_length
+
+def draw_detections(img, rects, thickness = 1):
+    """
+        INPUT :
+            img  : Gets the input frame
+            rect : Number from the regression layer (x0,y0,width,height)
+        OUTPUT:
+            count: Number of objects in a given frame
+            distance : Calculates the distance from the rect value
+
+    """
+
+    count = 0
+    distancei = 0.0
+    for x, y, w, h in rects:
+        print(len(rects))
+
+        if len(rects) >= 0: ### Increase the value of count if there are more than one rectangle in a given frame
+            count += 1
+        distancei = (2 * 3.14 * 180) / (w + h * 360) * 1000 + 3 ### Distance measuring in Inch
+        # print(distancei)
+        # distance = distancei * 2.54
+        # print(distance)
+        # the HOG detector returns slightly larger rectangles than the real objects.
+        # so we slightly shrink the rectangles to get a nicer output.
+        pad_w, pad_h = int(0.15*w), int(0.05*h)
+        cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), thickness)
+    return count, distancei
 
 
-#Load YOLO
-net = cv2.dnn.readNet("/home/kimchi/graspinglab/darknet/yolov3.weights","/home/kimchi/graspinglab/darknet/cfg/yolov3.cfg") # Original yolov3
-#net = cv2.dnn.readNet("yolov3-tiny.weights","yolov3-tiny.cfg") #Tiny Yolo
+#load YOLO
+#net = cv2.dnn.readNet("/home/kimchi/graspinglab/darknet/yolov3.weights","/home/kimchi/graspinglab/darknet/cfg/yolov3.cfg") # Original yolov3
+net = cv2.dnn.readNet("/home/kimchi/graspinglab/darknet/yolov3-tiny.weights","/home/kimchi/graspinglab/darknet/cfg/yolov3-tiny.cfg") #Tiny Yolo
+#net = cv2.dnn.readNet("/home/kimchi/graspinglab/darknet/backup/yolov3-tiny-shape_best.weights","/home/kimchi/graspinglab/darknet/cfg/yolov3-tiny-shape.cfg") #shape dataset 
+# set GPU run
+#net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+#net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+#load class file
 classes = []
-with open("/home/kimchi/graspinglab/darknet/data/coco.names","r") as f:
+with open("/home/kimchi/graspinglab/darknet/data/obj.names","r") as f:
     classes = [line.strip() for line in f.readlines()]
-
-
-# In[11]:
-
 
 print(classes)
 
 
-# In[12]:
-
 
 layer_names = net.getLayerNames()
 outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
-
-# In[ ]:
-
-
-
-
-
-# In[13]:
-
-
 colors= np.random.uniform(0,255,size=(len(classes),3))
 
 
-# In[ ]:
-
-
-
-
-
-# In[14]:
-
-
 #loading image
-cap=cv2.VideoCapture(0) #0 for 1st webcam
+cap=cv2.VideoCapture(-1) #0 for 1st webcam
 font = cv2.FONT_HERSHEY_PLAIN
 starting_time= time.time()
 frame_id = 0
 
 while True:
     _,frame= cap.read() # 
+    frame = cv2.resize(frame, (640, 640))
     frame_id+=1
     
     height,width,channels = frame.shape
     #detecting objects
-    #frame = cv2.resize(frame, (640, 640))
     blob = cv2.dnn.blobFromImage(frame,0.00392,(320,320),(0,0,0),True,crop=False)    
 
         
@@ -90,7 +94,7 @@ while True:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.3:
+            if confidence > 0.6:
                 #onject detected
                 center_x= int(detection[0]*width)
                 center_y= int(detection[1]*height)
@@ -112,11 +116,19 @@ while True:
 
     for i in range(len(boxes)):
         if i in indexes:
+            print(i)
+            #focal_length = (i * KNOWN_DISTANCE) / KNOWN_WIDTH #
+            #inches = distance_to_camera(KNOWN_WIDTH, focal_length, i) #
+            hog = cv2.HOGDescriptor()   #
+            hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())    #
+            found,w = hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05) #
+
             x,y,w,h = boxes[i]
             label = str(classes[class_ids[i]])
             confidence= confidences[i]
             color = colors[class_ids[i]]
             cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
+            
             cv2.putText(frame,label+" "+str(round(confidence,2)),(x,y+30),font,1,(255,255,255),2)
             
 
@@ -131,51 +143,4 @@ while True:
         break
     
 cap.release()    
-cv2.destroyAllWindows()    
-    
-    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-            
-        
-
-
-# In[ ]:
-
-
-
-
-
-# In[9]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+cv2.destroyAllWindows()
